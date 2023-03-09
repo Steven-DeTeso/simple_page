e846d759b15fc7dfe97c05b7da9c82e932b073e7
Steven DeTeso <sadeteso@gmail.com>
2023-03-08 19:08:32 -0500

    simple react page setup - init learn

---
+++ Header.js
@@ -0,0 +1,16 @@
+function Header () {
+    return (
+        <header>
+            <nav className="nav">
+                <h1>Img</h1>
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
---
+++ MainContent.js
@@ -0,0 +1,13 @@
+function MainContent () {
+    return (
+        <div>
+            <h2>Reason I'm excited to learn React</h2>
+            <ol>
+                <li>React is a hireable skill!</li>
+                <li>React is fun and it can be cool to build cool things</li>
+            </ol>
+        </div>
+    )
+}
+
+export default MainContent
